02048495096ecf37802acc7839efa57feb1c0c10
jordanlingao <jordanlingao@gmail.com>
2023-04-08 22:55:24 -0400

    Completed Database class

--- app/data.py
+++ app/data.py
@@ -1,3 +1,5 @@
+'''Module containing a Database class to hold the specs of randomized monsters.'''
+
 from os import getenv
 
 from certifi import where
@@ -8,18 +10,47 @@ from pymongo import MongoClient
 
 
 class Database:
+    '''
+    Class that holds a MongoDB database and collection of MonsterLab.Monster objects.
+    '''
+
+    def __init__(self):
+        # Connect to database using credentials from .env
+        load_dotenv()
+        database = MongoClient(
+            getenv("DB_URL"),
+            tlsCAFile=where())["Bandersnatch"]
+
+        # Create the Monsters collection
+        if "Monsters" not in database.list_collection_names():
+            database.create_collection("Monsters")
+        self.collection = database.get_collection("Monsters")
 
     def seed(self, amount):
-        pass
+        '''Inserts a given number of MonsterLab.Monster objects into the Monster collection.'''
+        return self.collection.insert_many(
+            [Monster().to_dict() for i in range(amount)])
 
     def reset(self):
-        pass
+        '''Deletes all documents in the collection.'''
+        self.collection.delete_many({})
 
     def count(self) -> int:
-        pass
+        '''Returns number of documents in the collection.'''
+        return self.collection.count_documents({})
 
     def dataframe(self) -> DataFrame:
-        pass
+        '''Returns pandas DataFrame of documents in the collection.'''
+        return DataFrame(self.collection.find({}))
 
     def html_table(self) -> str:
-        pass
+        '''Returns html table of documents in the collection.'''
+        if self.count() == 0:
+            return None
+        return DataFrame(self.collection.find({})).to_html()
+
+
+if __name__ == "__main__":
+    db = Database()
+    db.seed(10)
+    print(db.dataframe())
